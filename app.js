9a5bdc37127d81d0331fae863987dbe8e9975e9b
MarceloHey <marcelotoniolo.h@gmail.com>
2022-12-12 14:32:27 -0300

    git init

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ app.use('/usuario', usuarioRoutes)
 app.use(express.static(path.join(__dirname, 'public')))
 
 //OTHER
-const PORT = 8081
+const PORT = process.env.PORT || 8081
 app.listen(PORT, () => {
     console.log('Servidor rodando')
 })
